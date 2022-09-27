edf711bfc12d5e6b4180f5276c18e2777bb5a68c
Jose Flores <flores.jdfr@gmail.com>
2022-09-27 00:00:41 -0600

    Update profile information

--- Serigramall.Web.Provider/src/pages/Profile.jsx
+++ Serigramall.Web.Provider/src/pages/Profile.jsx
@@ -1,68 +1,15 @@
-import { useAuth0, User } from "@auth0/auth0-react";
-import { Container, Form } from "react-bootstrap";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Container } from "react-bootstrap";
+import InformationForm from "../components/profile/InformationForm";
 import Loading from "../components/utils/Loading";
 
 function Profile() {
-  const { isAuthenticated, isLoading, user } = useAuth0();
-
-  function InformationForm() {
-    return (
-      <>
-        <Form>
-          <Form.Group>
-            <Form.Label>Company Name</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Not specified"
-              disabled={true}
-              value={user.name}
-            />
-          </Form.Group>
-          <Form.Group className="mt-3">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Not specified"
-              disabled={true}
-              value={user.email}
-            />
-          </Form.Group>
-          <Form.Group className="mt-3">
-            <Form.Label>Street Address</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Not specified"
-              disabled={true}
-              value={user.user_metadata.address}
-            />
-          </Form.Group>
-          <Form.Group className="mt-3">
-            <Form.Label>Phone Number</Form.Label>
-            <Form.Control
-              type="number"
-              placeholder="Not specified"
-              disabled={true}
-              value={user.user_metadata.phone}
-            />
-          </Form.Group>
-          <Form.Group className="mt-3">
-            <Form.Label>Provider Type</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Not specified"
-              disabled={true}
-              value={user.user_metadata.provider == "1" ? "Serigraphy" : "Products"}
-            />
-          </Form.Group>
-        </Form>
-      </>
-    );
-  }
+  const { isLoading, user } = useAuth0();
 
   return (
     <Container>
       <h2 className="mb-3">Profile Information</h2>
-      {isLoading ? <Loading /> : <InformationForm />}
+      {isLoading ? <Loading /> : <InformationForm user={user} />}
     </Container>
   );
 }
