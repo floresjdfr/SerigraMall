3b6eba8718922b9f230957787bab7a3fbb4e3d45
Rando97 <46728173+Rando147@users.noreply.github.com>
2022-10-08 16:34:11 -0600

    Update App.jsx

--- Serigramall.Web.Provider/src/App.jsx
+++ Serigramall.Web.Provider/src/App.jsx
@@ -23,14 +23,15 @@ function App() {
           <Route path="/" element={<Index />} />
 
           {/* Protected routes */}
-          <Route path="/product" element={<ProtectedRoute component={Product} forceCompleteProfile={true} scopes={["manage:services"]} />} />
+          {/*<Route path="/product" element={<ProtectedRoute component={Product} forceCompleteProfile={true} scopes={["manage:services"]} />} />*/}
+          <Route path="/product" element={Product}/>
           <Route path="/manage-profile" element={<ProtectedRoute component={Profile} scopes={["manage:services"]} />} />
 
           {/*Error Routes */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
-    </>
+    </GlobalProvider>
   );
 }
 
