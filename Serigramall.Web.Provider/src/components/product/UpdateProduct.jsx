f4896edc9a5ff63c8e3bfe275bfe7de4899b0cc5
Jose Flores <flores.jdfr@gmail.com>
2022-10-27 18:29:08 -0600

    Working on update products

--- Serigramall.Web.Provider/src/components/product/UpdateProduct.jsx
+++ Serigramall.Web.Provider/src/components/product/UpdateProduct.jsx
@@ -1,21 +1,27 @@
 
-import React, { useEffect,useContext, useState } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { Form, Button, Modal } from "react-bootstrap";
 import productApi from '../../services/api/productApi';
-import { ProductContext } from "../../Pages/Product";
 import { GlobalContext } from "../../contexts/GlobalContext";
+import { ProductsContext } from "../../contexts/ProductsContext";
+import Select from 'react-select';
 
 const UpdateProduct = ({ product }) => {
-  const [productState, setProductState] = useState("");
-  const { setShowToast, setToastHeader, setToastBody } =
-    useContext(GlobalContext);
+  const { setShowToast, setToastHeader, setToastBody, setIsLoading } = useContext(GlobalContext);
+  const { setProductsState } = useContext(ProductsContext);
 
   const [show, setShow] = useState(false);
   const [inputs, setInputs] = useState({});
-
+  const [fileImage, setFileImage] = useState();
+  const [selectedState, setSelectedState] = useState(`${product.productState}`);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  useEffect(() => {
+    _loadProductInfo();
+  }, []);
+
 
   const handleOnChange = (event) => {
     const name = event.target.name;
@@ -25,66 +31,99 @@ const UpdateProduct = ({ product }) => {
     setInputs(inputs);
   };
 
-  useEffect(() => {
-    _loadProductInfo();
-  }, []);
+  //ACTIVE, SOLDOUT, DESCONTINUED, DEFAULT, NEW
+  const states = [
+    { value: "0", label: "Active" },
+    { value: "1", label: "Sold Out" },
+    { value: "2", label: "Discontinued" },
+    { value: "3", label: "Default" },
+    { value: "4", label: "New" }
+  ];
+
+  const getUserSelectedState = (product) => {
+    const selectedState = states
+      .filter(state => state.value === `${product.productState}`)
+      .reduce((acc, curr) => {
+        return curr;
+      }, {});
+    return selectedState;
+  }
 
   function _loadProductInfo() {
     var name = "Description"
     var value = product.description
-    _loadProductInputs(name,value);
+    _loadProductInputs(name, value);
 
     name = "Tax"
     value = product.baseTax
-    _loadProductInputs(name,value);
+    _loadProductInputs(name, value);
 
     name = "Price"
     value = product.basePrice
-    _loadProductInputs(name,value);
+    _loadProductInputs(name, value);
 
     name = "State"
     value = product.ProductState
-    _loadProductInputs(name,value);
+    _loadProductInputs(name, value);
   }
 
-  function _loadProductInputs(pname,pvalue) {
+  function _loadProductInputs(pname, pvalue) {
     const name = pname;
     const value = pvalue;
     inputs[name] = value;
     setInputs(inputs);
   }
 
+  function handleImageChange(event) {
+    console.log(event.target.files[0]);
+    //setFileImage(URL.createObjectURL(event.target.files[0]))
+    getAnswer(event.target.files[0]);
+    setFileImage(blobToBase64(event.target.files[0]));
+    console.log(fileImage);
+  }
+
+  const getAnswer = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setAns(data);
+  };
+
   const handleOnSubmit = (event) => {
 
     event.preventDefault();
+    setIsLoading(true);
 
     const updatedProduct = {
       Id: product.id,
       Description: inputs.Description,
       ProviderId: product.providerID,
       ProductName: product.productName,
-      RegistryDate: product.registryDate,
       BasePrice: inputs.Price,
       BaseTax: inputs.Tax,
       Image: product.image,
       ProductType: product.productType,
-      Costumizations: product.costumizations,
-      ProductState: parseInt(inputs.State,10)
+      ProductState: inputs.State
     };
-
 
     productApi.put(updatedProduct)
       .then((_) => productApi.getAll())
-      .then((response) => {(response === 500) ?  throwError(): setProductsState(response)})
+      .then((response) => {
+        (response === 500) ? throwError() : setProductsState(response.data)
+      })
       .catch((_) => {
         setToastHeader("Error");
         setToastBody("An error ocurred while trying to retrive the requested Items");
         setShowToast(true);
-        setIsLoading(false);
-      });
-    handleClose();
 
+      })
+      .finally(() => setIsLoading(false));
+    handleClose();
   };
+
+  const handleChange = (selectedOption) => {
+    setSelectedState(selectedOption);
+  };
+
   return (
     <div className="row">
       <div className="col">
@@ -103,7 +142,6 @@ const UpdateProduct = ({ product }) => {
                 <Form.Control
                   type="text"
                   name="Name"
-                  disabled={true}
                   defaultValue={product.productName}
                   placeholder="Name"
                 />
@@ -115,16 +153,6 @@ const UpdateProduct = ({ product }) => {
                   name="Description"
                   defaultValue={product.description}
                   placeholder="Description"
-                />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>Registry date</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="Date"
-                  disabled={true}
-                  defaultValue={product.registryDate}
-                  placeholder="Date"
                 />
               </Form.Group>
               <Form.Group className="mb-3">
@@ -147,16 +175,20 @@ const UpdateProduct = ({ product }) => {
               </Form.Group>
               <Form.Group className="mb-3">
                 <Form.Label>State</Form.Label>
+                <Select options={states} defaultValue={getUserSelectedState(product)} onChange={handleChange} />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Image</Form.Label>
                 <Form.Control
-                  type="text"
-                  name="State"
-                  defaultValue={product.productState}
-                  placeholder="State"
+                  type="file"
+                  name="Image"
+                  placeholder="Image"
+                  onChange={handleImageChange}
                 />
               </Form.Group>
             </Modal.Body>
             <Modal.Footer>
-              <Button className="float-start"variant="secondary" onClick={handleClose}>
+              <Button className="float-start" variant="secondary" onClick={handleClose}>
                 Close
               </Button>
               <Button className="float-end" type="submit" variant="primary">
