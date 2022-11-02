0ccd23928f76c7b534267db9ac75ad5757e316cf
lawny04 <lawny.vargas@gmail.com>
2022-11-01 20:21:13 -0600

    arreglo del bug de eliminar

--- Serigramall.Web.Client/src/components/carrito/CartContext.jsx
+++ Serigramall.Web.Client/src/components/carrito/CartContext.jsx
@@ -2,75 +2,73 @@ import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext();
 
-export const CartProvider = ({children})=> {
-    const [cartItems, setCartItems] = useState(()=>{
+export const CartProvider = ({ children }) => {
+    const [cartItems, setCartItems] = useState(() => {
         try {
             const productosEnLocalStorage = localStorage.getItem("cartProducts");
-            return productosEnLocalStorage ? JSON.parse(productosEnLocalStorage):[];
-          
+            return productosEnLocalStorage ? JSON.parse(productosEnLocalStorage) : [];
+
         } catch (error) {
             return [];
         }
     });
-    useEffect(() =>{
+    useEffect(() => {
         try {
-            console.log("¨ls A: ");
-        console.log(cartItems);
-        localStorage.setItem("cartProducts",JSON.stringify(cartItems));
-            
-        } catch (error) {
-            console.log("¨ls A: ");
-        }
-    },[cartItems]);
+            localStorage.setItem("cartProducts", JSON.stringify(cartItems));
 
-    const addItemToCart = (product)=>{
-        console.log("addItemToCart");
-        console.log(product);
+        } catch (error) {
+            console.log("error");
+        }
+    }, [cartItems]);
+
+    const addItemToCart = (product) => {
         const inCart = cartItems.find(
-            (productInCart)=>productInCart.id==product.id); 
-   
+            (productInCart) => productInCart.id == product.id);
+
         if (inCart) {
             setCartItems(
-                cartItems.map((productInCart)=>{
-                    if (productInCart.id==product.id) {
-                        return {...inCart,amount: inCart.amount+1};
+                cartItems.map((productInCart) => {
+                    if (productInCart.id == product.id) {
+                        return { ...inCart, amount: inCart.amount + 1 };
                     } else {
                         return productInCart;
                     }
                 })
             );
-        }else {
-            setCartItems([...cartItems,{...product,amount:1}]);
-        } 
-        
+        } else {
+            setCartItems([...cartItems, { ...product, amount: 1 }]);
+        }
+
     };
-    
+
     const deleteItemToCart = (product) => {
-        const inCart =cartItems.find(
-            (productInCart)=>productInCart.id===product.id
+        const inCart = cartItems.find(
+            (productInCart) => productInCart.id === product.id
         );
-        if(inCart.amount===1){
+        if (inCart.amount === 1) {
             setCartItems(
-                cartItems.filter(productInCart =>productInCart.id !== product.id)
+                cartItems.filter(productInCart => productInCart.id !== product.id)
             );
-        }else{
-            setCartItems((productInCart) =>{
-                if(productInCart.id === product.id){
-                    return {...inCart,amount:inCart.amount-1};
-                }
-                else return productInCart;
-        });
+        } else {
+            setCartItems(
+                cartItems.map((productInCart) => {
+                    if (productInCart.id === product.id) {
+                        return { ...inCart, amount: inCart.amount - 1 };
+                    }
+                    else return productInCart;
+
+                }));
         }
     };
 
     return (
         /* Envolvemos el children con el provider y le pasamos un objeto con las propiedades que necesitamos por value */
         <CartContext.Provider
-        value={{ cartItems,addItemToCart, deleteItemToCart }}
+            value={{ cartItems, addItemToCart, deleteItemToCart }}
         >
-        {children}
-      </CartContext.Provider>
+            {children}
+        </CartContext.Provider>
     );
-        
+
 };
 export default CartContext;
