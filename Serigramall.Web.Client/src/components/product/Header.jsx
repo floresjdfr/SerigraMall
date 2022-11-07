99b31a1158a95c9ecc866fe884b75b3f53dab8cd
Jose Flores <flores.jdfr@gmail.com>
2022-11-07 12:14:27 -0600

    Testing styles

--- Serigramall.Web.Client/src/components/product/Header.jsx
+++ Serigramall.Web.Client/src/components/product/Header.jsx
@@ -1,13 +1,25 @@
-import { Form, Button, Modal } from "react-bootstrap";
+import { Form, Button, Modal, Row, Col } from "react-bootstrap";
 
 const Header = () => {
     return (
-        <div className='p-5 text-center bg-muted text-muted'>
-            <h2 className='mb-3 text-muted'>Products</h2>
-            <div>
-                <input className="float-center "type="text"/>
-                <Button className="float-center " variant="secondary">Seach</Button>
-            </div>         
+        <div className='header mb-3'>
+            <h2 className='text-muted'>Products</h2>
+            <Form>
+                <div className="row px-4">
+                    <div className="col-5 p-0 pe-1">
+                        <Form.Select>
+                            <option>All providers</option>
+                            <option value="1">Provider 1</option>
+                        </Form.Select>
+                    </div>
+                    <div className="col-5 p-0">
+                        <Form.Control type="text" placeholder="Search product"></Form.Control>
+                    </div>
+                    <div className="col-2 p-0">
+                        <Button variant="primary" type="submit">Search</Button>
+                    </div>
+                </div>
+            </Form>
         </div>
     );
 }
