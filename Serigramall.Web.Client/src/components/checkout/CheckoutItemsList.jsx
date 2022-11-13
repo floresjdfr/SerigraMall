3659006c40dde427c60ff676ff62144501e77450
Jose Flores <flores.jdfr@gmail.com>
2022-11-13 17:13:26 -0600

    Still working on responsive

---
+++ Serigramall.Web.Client/src/components/checkout/CheckoutItemsList.jsx
@@ -0,0 +1,12 @@
+import { Table } from "react-bootstrap"
+
+export default function CheckoutItemsList({ children }) {
+    return
+    <>
+        <h1>Shoppig Cart</h1>
+        <Table>
+            {children}
+        </Table>
+    </>
+
+} 
