d4dd70754420152a431ee51eff65aa69e2cea157
Jose Flores <flores.jdfr@gmail.com>
2022-11-17 11:19:32 -0600

    Api url fix

--- Serigramall.Web.Client/src/services/api/orderApi.js
+++ Serigramall.Web.Client/src/services/api/orderApi.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const plural = "Orders";
 const single = "Order";
 const url = "Order";
-const BASE_URL = `${import.meta.env.VITE_SERVER_API_URL}/api`;
+const BASE_URL = import.meta.env.VITE_SERVER_API_URL;
 
 const apiOrders = new ApiCore({
     getAll: false,
--- Serigramall.Web.Client/src/services/api/utilities/provider.js
+++ Serigramall.Web.Client/src/services/api/utilities/provider.js
@@ -4,8 +4,7 @@ import axios from 'axios';
 
 // Define your api url from any source.
 // Pulling from your .env file when on the server or from localhost when locally
-//const BASE_URL = import.meta.env.VITE_SERVER_API_URL;
-const BASE_URL = "https://localhost:44355/api";
+const BASE_URL = import.meta.env.VITE_SERVER_API_URL;
 
 /** @param {string} resource */
 const getAll = (resource) => {
